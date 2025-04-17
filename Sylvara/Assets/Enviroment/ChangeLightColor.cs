e6f102d4ebfd051760ee0fc23562b24a1f04c713
CristoYisus69 <crisnuva1427@gmail.com>
2025-04-16 22:21:52 -0600

    enviroment 70%

--- Sylvara/Assets/Enviroment/ChangeLightColor.cs
+++ Sylvara/Assets/Enviroment/ChangeLightColor.cs
@@ -4,42 +4,43 @@ public class ChangeLightColor : MonoBehaviour
 {
     public Light sceneLight; // La luz que quieres cambiar
     public Color targetColor = Color.blue; // El color al que quieres que cambie la luz
+    public float targetIntensity = 5.0f; // La intensidad a aplicar
+    public float originalIntensity = 1.0f; // Intensidad original
     public Color originalColor = Color.white; // El color original de la luz
     public string playerTag = "Player"; // Etiqueta que debe tener el personaje
 
     private void OnTriggerEnter(Collider other)
     {
-        // Verifica si el objeto que entra en el trigger es el personaje
         if (other.CompareTag(playerTag))
         {
-            // Cambiar el color de la luz
             if (sceneLight != null)
             {
                 sceneLight.color = targetColor;
+                sceneLight.intensity = targetIntensity;
             }
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        // Mientras el personaje esté dentro de la zona, mantén el color de la luz
         if (other.CompareTag(playerTag))
         {
             if (sceneLight != null)
             {
                 sceneLight.color = targetColor;
+                sceneLight.intensity = targetIntensity;
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // Cuando el personaje salga de la zona, vuelve a su color original
         if (other.CompareTag(playerTag))
         {
             if (sceneLight != null)
             {
                 sceneLight.color = originalColor;
+                sceneLight.intensity = originalIntensity;
             }
         }
     }
